28b95b81f135920eb1dcfbe21894ed585fdc621c
Vineal Viji Varghese <vinealviji@gmail.com>
2021-03-01 14:46:04 +0530

    Initial Commit

---
+++ PracticeApp1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PracticeApp1
+//
+//  Created by Vineal Viji Varghese on 01/03/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PracticeApp1/PracticeApp1App.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeApp1App.swift
+//  PracticeApp1
+//
+//  Created by Vineal Viji Varghese on 01/03/21.
+//
+
+import SwiftUI
+
+@main
+struct PracticeApp1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
